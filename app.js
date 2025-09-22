98d42aa330ebc46269e8939a06d4fe598ba8b3c4
K Narayan Reddy <narayanreddy355@gmail.com>
2025-09-22 20:33:40 +0530

    change url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://guileless-moxie-73d900.netlify.app/",
     credentials: true, 
 }))
 
