abd46371a1d5688f999d1955355975a4b3c3c305
K Narayan Reddy <narayanreddy355@gmail.com>
2025-09-22 16:35:23 +0530

    All files are aded

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ import dotenv from "dotenv"
 import cors from "cors"
 
 const app = express()
-const port = process.env.PORT || 5000
 dotenv.config()
+
+const port = process.env.PORT || 5000
 
 app.use(express.json())
 
